fe23f20419dcac62bb9f23a66f6728e5c6c5c447
omar.guemmi@gmail.com <omar.guemmi@gmail.com>
2018-02-26 21:20:30 +0100

    initial upload

---
+++ 04-decouverte/decouverte.sh
@@ -0,0 +1,16 @@
+wget http://www.lienmini.fr/a152-cookies
+mv a152-cookies a152-cookies.zip
+unzip a152-cookies.zip
+
+wget http://www.lienmini.fr/a152-carre
+mv a152-carre a152-carre.zip
+unzip a152-carre.zip
+
+wget http://www.lienmini.fr/a152-perimetre
+mv a152-perimetre a152-perimetre.zip
+unzip a152-perimetre.zip
+
+
+wget http://www.lienmini.fr/a152-aspirateur
+mv a152-aspirateur a152-aspirateur.zip
+unzip a152-aspirateur.zip
